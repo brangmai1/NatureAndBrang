d97a8b618e8ef00520c1beacfce9848ab659d8d6
Brang Mai <brangmai100@gmail.com>
2023-02-13 12:12:35 -0500

    Added new features on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,10 @@ export default function Header() {
     return (
         <div className="header">
             <h1 className="header--title">BRANG MAI</h1>
+            <div className="header--right">
+                <div className="image-gallery">Image Gallery</div>
+                <div className="about-me">About Me</div>
+            </div>
         </div>
     )
 }
--- src/components/TravellingData.js
+++ src/components/TravellingData.js
@@ -6,7 +6,7 @@ export default [
         googleMap: "https://www.google.com/maps/@43.7613559,-110.6971179,10z?hl=en",
         placeName: "Grand Tetons National Park",
         date: "September - October, 2022",
-        note: "Beautiful place Lovely, Loved it.Oscar and Emmy winner Kevin Costner is the marquee attraction of the ensemble cast in this drama series, starring as the patriarch of a powerful, complicated family of ranchers."
+        note: "The Grand Teton National Park is absoblutely stunning. The beauty of the park and the majestic grand tetons take my breath away everytime I visit the park. They say that there are many wild lives such as moose, elks, goats, bear and many more. But, I am just overwhemed with the beauty of the park itself. I would get out of my car, stare at the rocky mountains and think what the feeling would be to stand at the top the highiest one. I would never get tired of looking at the rookies, the lake and everything about this park. I love the Grand Teton National Park."
     },
     {
         id: 2,
@@ -15,7 +15,7 @@ export default [
         googleMap: "https://www.google.com/maps/@44.5294271,-110.470409,10z?hl=en",
         placeName: "Bisons in Yellostone National Park",
         date: "September - October, 2022",
-        note: "Beautiful place Lovely, Loved it.Beautiful place Lovely, Loved it.Oscar and Emmy winner Kevin Costner is the marquee attraction of the ensemble cast in this drama series, starring as the patriarch of a powerful, complicated family of ranchers."
+        note: "This part of Yellow Stone National Park is so peaceful and relaxing. The lanscape, the winding river and the bisons would calm you down from any type of worries, anxieties and concerns in your life. I have no word to describe the beauty of Yellow Stone National Park."
     },
     {
         id: 3,
@@ -24,7 +24,7 @@ export default [
         googleMap: "https://www.google.com/maps/@36.9867152,-110.1723535,12z?hl=en",
         placeName: "National Manument Valley",
         date: "September - October, 2022",
-        note: "Beautiful place Lovely, Loved it. Beautiful place Lovely, Loved it.Oscar and Emmy winner Kevin Costner is the marquee attraction of the ensemble cast in this drama series, starring as the patriarch of a powerful, complicated family of ranchers."
+        note: "National Manument Valley is a very strange and scary place for me because the red rocks are stading like temples in the middle of desert. But, I have met some amazing native americans during my last visit."
     },
     {
         id: 4,
@@ -33,7 +33,7 @@ export default [
         googleMap: "https://www.google.com/maps/@44.5294271,-110.470409,10z?hl=en",
         placeName: "The Grand Canyon of Yellowstone",
         date: "September - October, 2022",
-        note: "Beautiful place Lovely, Loved it. Beautiful place Lovely, Loved it.Oscar and Emmy winner Kevin Costner is the marquee attraction of the ensemble cast in this drama series, starring as the patriarch of a powerful, complicated family of ranchers."
+        note: "The grandfather of all national parks around the world, Yellow Stone National Park, is the richest one in nature beauty wealth. This cayon fall is one of the most spectacular nature beauties. Standing at the top of this high waterfall made me feel like I am in another planet. Looking down to the base of this waterfall from the top was somewhat intimidating, but it was also very satisfying."
     },
     {
         id: 5,
@@ -42,7 +42,7 @@ export default [
         googleMap: "https://www.google.com/maps/@43.7613559,-110.6971179,10z?hl=en",
         placeName: "Dinosaur National Manuments",
         date: "September - October, 2022",
-        note: "Beautiful place Lovely, Loved it.Beautiful place Lovely, Loved it.Oscar and Emmy winner Kevin Costner is the marquee attraction of the ensemble cast in this drama series, starring as the patriarch of a powerful, complicated family of ranchers."
+        note: "Dinosaur National Manument is a must visit place if you love nature just like I do. You have to drive through a narrow path surrounded by the cliffs on both left and right to get to this place. One of my friends, who I was travelling with, creamed out of his panic attack while we were driving through. There are beautiful nature at every point in this park."
     },
     {
         id: 6,
@@ -51,7 +51,7 @@ export default [
         googleMap: "https://www.google.com/maps/@43.7613559,-110.6971179,10z?hl=en",
         placeName: "Dune",
         date: "September - October, 2022",
-        note: "Beautiful place Lovely, Loved it.Beautiful place Lovely, Loved it.Oscar and Emmy winner Kevin Costner is the marquee attraction of the ensemble cast in this drama series, starring as the patriarch of a powerful, complicated family of ranchers."
+        note: "No explanation is needed to see this God's creation. Beautiful place in Utah, USA."
     },
     {
         id: 7,
@@ -60,6 +60,6 @@ export default [
         googleMap: "https://www.google.com/maps/@44.5294271,-110.470409,10z?hl=en",
         placeName: "The Winding River in Yellowstone",
         date: "September - October, 2022",
-        note: "Beautiful place Lovely, Loved it. Beautiful place Lovely, Loved it.Oscar and Emmy winner Kevin Costner is the marquee attraction of the ensemble cast in this drama series, starring as the patriarch of a powerful, complicated family of ranchers."
+        note: "Yellow Stone National Park"
     }
 ]
