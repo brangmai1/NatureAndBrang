870daa786b8e68a9764d7042f847b78f44ccbe3a
Brang Mai <brangmai100@gmail.com>
2023-02-22 00:20:02 -0500

    Readjusted travel-journey.js file

--- src/components/TravelJournal.js
+++ src/components/TravelJournal.js
@@ -4,20 +4,19 @@ import locationIcon from "../images/icons/location-icon.webp"
 export default function TravelJournal(props) {
     return (
         <div className="travel-journal">
-            <img className="travel-image" src={require(`../images/${props.data.image}`)} alt="location"></img>
-            <article className="travel-info">
+            <div className="journal-left">
+                <img className="travel-image" src={require(`../images/${props.data.image}`)} alt="location"></img>
                 <section className="location-info">
                     <img className="location-info--icon" src={locationIcon}alt="location-icon"></img>
                     <h5 className="location-info--country">{props.data.location}</h5>
                     <a className="location-info--map" href={props.data.googleMap} target="_blank" rel="noreferrer">View on Google Maps</a>
                 </section>
-                <section  className="travel-memo">
-                    <h2>{props.data.placeName}</h2>
-                    <h5>{props.data.date}</h5>
-                    <p>{props.data.note}</p>
-                </section>
-            </article>
-            
+            </div>            
+            <section  className="travel-memo">
+                <h2>{props.data.placeName}</h2>
+                <h5>{props.data.date}</h5>
+                <p>{props.data.note}</p>
+            </section>            
         </div>
 
     )
